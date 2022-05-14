a9bf4d2ab10ac5167043de8b7f337fdaf89380ae
qianlong98 <1915587099@qq.com>
2022-05-14 13:46:30 +0800

    second commit

--- src/main/java/cn/sdfirefly/git/GitTest.java
+++ src/main/java/cn/sdfirefly/git/GitTest.java
@@ -7,5 +7,7 @@ package cn.sdfirefly.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+
     }
 }
