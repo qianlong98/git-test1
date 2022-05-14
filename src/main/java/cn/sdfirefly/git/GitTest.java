5c1a1e909d7b28983b469c69c781508a7126bd49
qianlong98 <1915587099@qq.com>
2022-05-14 13:43:17 +0800

    first commit

---
+++ src/main/java/cn/sdfirefly/git/GitTest.java
@@ -0,0 +1,11 @@
+package cn.sdfirefly.git;
+
+/**
+ * @author sdfirefly
+ * @create 2022/5/14--13:38
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
